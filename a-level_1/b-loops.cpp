9ae513c5b5d8daefd7657e1d89709fdf3af6ff5c
Mina  Faried <mina.el.dihabi3.6.9@gmail.com>
2022-09-29 16:08:13 +0200

    loops

--- a-level_1/b-loops.cpp
+++ a-level_1/b-loops.cpp
@@ -14,8 +14,8 @@ int main()
 {
     // * 1- find pair of numbers equal sum
     cout << "find pair of numbers equal sum: " << endl;
-//want to find 2 num equls sum
-    int r1 = 70, r2 = 60, sum = 70;
+    // want to find 2 num equals sum
+    int r1 = 10, r2 = 20, sum = 6;
     int counts = 0;
 
     for (int i = 1; i <= r1; i++)
@@ -28,10 +28,42 @@ int main()
             i is ++
             j is --
             */
-            cout << i << " + " << j << " = " << sum << endl;
+            // cout << i << " + " << j << " = " << sum << endl;
             counts++;
         }
     }
     cout << counts << endl;
+
     cout << "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" << endl;
+    // * 2- find Triple of numbers less than or equal sum
+    cout << "find Triple of numbers less than or equal sum: " << endl;
+
+    r1 = 100, r2 = 200;
+    int r3 = 20;
+    counts = 0;
+
+    for (int i = 1; i <= r1; i++)
+        for (int j = i; j <= r1; j++)
+        {
+            int k = i + j;
+
+            if (1 <= k && k <= r3)
+            {
+                // cout << i << " + " << j << " <= " << k << endl;
+                counts += r3 - k + 1;
+            }
+        }
+
+    cout << counts << endl;
+
+    cout << "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" << endl;
+    // * 2- fibonacci in different way
+    cout << "fibonacci in different way: " << endl;
+
+    int limit = 10;
+
+    for (int a = 0, b = 1, c = -1, i = 0; i < limit; i++, c = a + b, a = b, b = c)
+        cout << a << " ";
+
+    cout << endl;
 }
---
+++ a-level_1/c-array.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+#include <string>
+#include <tuple>
+#include <cmath>
+#include <cstring>
+#include <vector>
+#include <stack>
+#include <queue>
+using namespace std;
+#define lol long long int
+int main()
+{
+    // * 1- find pair of numbers equal sum
+    cout << "find pair of numbers equal sum: " << endl;
+}
